c1f73f83a9244678d73efa868892f88bac4afc60
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-26 12:52:53 +0100

    896 | Monotonic Array | refactoring

--- src/main/kotlin/leetcode/problems/redundant_connection_896/MonotonicArray.kt
+++ src/main/kotlin/leetcode/problems/redundant_connection_896/MonotonicArray.kt
@@ -4,16 +4,24 @@ class Solution {
     fun isMonotonic(values: IntArray): Boolean {
         if (values.isEmpty()) return true
 
-        val initialValue = values[0]
-        val increaseResult = values.toList().drop(1).fold(Pair(true, initialValue), { acc, value ->
-            Pair(acc.first && value >= acc.second, value)
-        })
+        return isIncreasing(values) || isDescreasing(values)
+    }
 
+    private fun isDescreasing(values: IntArray): Boolean {
+        val initialValue = values[0]
 
         val decreaseResult = values.toList().drop(1).fold(Pair(true, initialValue), { acc, value ->
             Pair(acc.first && value <= acc.second, value)
         })
+        return decreaseResult.first
+    }
 
-        return increaseResult.first || decreaseResult.first
+    private fun isIncreasing(values: IntArray): Boolean {
+        val initialValue = values[0]
+
+        val increaseResult = values.toList().drop(1).fold(Pair(true, initialValue), { acc, value ->
+            Pair(acc.first && value >= acc.second, value)
+        })
+        return increaseResult.first
     }
 }
