262305a76a31ac6e868c69a7f04fc451f859e21f
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-26 12:48:01 +0100

    896 | Monotonic Array | first cut implementatio

--- src/main/kotlin/leetcode/problems/redundant_connection_896/MonotonicArray.kt
+++ src/main/kotlin/leetcode/problems/redundant_connection_896/MonotonicArray.kt
@@ -2,6 +2,18 @@ package leetcode.problems.redundant_connection_896
 
 class Solution {
     fun isMonotonic(values: IntArray): Boolean {
-        return true
+        if (values.isEmpty()) return true
+
+        val initialValue = values[0]
+        val increaseResult = values.toList().drop(1).fold(Pair(true, initialValue), { acc, value ->
+            Pair(acc.first && value >= acc.second, value)
+        })
+
+
+        val decreaseResult = values.toList().drop(1).fold(Pair(true, initialValue), { acc, value ->
+            Pair(acc.first && value <= acc.second, value)
+        })
+
+        return increaseResult.first || decreaseResult.first
     }
 }
--- src/test/kotlin/leetcode/problems/redundant_connection_896/MonotonicArrayTest.kt
+++ src/test/kotlin/leetcode/problems/redundant_connection_896/MonotonicArrayTest.kt
@@ -11,17 +11,37 @@ class MonotonicArrayTest {
     }
 
     @Test
-    fun `single element array is always monotonic`() {
+    fun `1 element array is always monotonic`() {
         assertThat(Solution().isMonotonic(intArrayOf(1))).isTrue()
     }
 
     @Test
-    fun `two element increasing monotonic array`() {
+    fun `2 element increasing monotonic array`() {
         assertThat(Solution().isMonotonic(intArrayOf(1, 2))).isTrue()
     }
 
     @Test
-    fun `two element decreasing monotonic array`() {
-        assertThat(Solution().isMonotonic(intArrayOf(2, 1))).isTrue()
+    fun `3 element increasing monotonic array`() {
+        assertThat(Solution().isMonotonic(intArrayOf(1, 2, 3))).isTrue()
     }
+
+    @Test
+    fun `3 element decreasing monotonic array`() {
+        assertThat(Solution().isMonotonic(intArrayOf(3, 2, 1))).isTrue()
+    }
+
+    @Test
+    fun `two equal elements is a monotonic array`() {
+        assertThat(Solution().isMonotonic(intArrayOf(1, 1))).isTrue()
+    }
+
+    @Test
+    fun `more comprehensive tests - sample date`() {
+        assertThat(Solution().isMonotonic(intArrayOf(1,2,2,3))).isTrue()
+        assertThat(Solution().isMonotonic(intArrayOf(6,5,4,4))).isTrue()
+        assertThat(Solution().isMonotonic(intArrayOf(1,3,2))).isFalse()
+        assertThat(Solution().isMonotonic(intArrayOf(1,2,4,5))).isTrue()
+        assertThat(Solution().isMonotonic(intArrayOf(1,1,1))).isTrue()
+    }
+
 }
