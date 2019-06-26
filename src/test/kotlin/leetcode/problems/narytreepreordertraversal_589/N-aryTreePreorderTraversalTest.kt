5e8d0d2f58043d9cd6136581659d08014c217fd2
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-26 16:02:49 +0100

    589 | N-ary Tree Preorder Traversal | added a more comprehensive test

--- src/test/kotlin/leetcode/problems/narytreepreordertraversal_589/N-aryTreePreorderTraversalTest.kt
+++ src/test/kotlin/leetcode/problems/narytreepreordertraversal_589/N-aryTreePreorderTraversalTest.kt
@@ -16,5 +16,12 @@ class `N-aryTreePreorderTraversalTest` {
         assertThat(Solution().preorder(Node(1, listOf(Node(2, emptyList()))))).isEqualTo(listOf(1, 2))
     }
 
-
+    @Test
+    fun `parent and 2 level children - sample daa`() {
+        assertThat(Solution()
+                .preorder(
+                        Node(1,
+                                listOf(Node(3, listOf(Node(5, emptyList()), Node(6, emptyList()))), Node(2, emptyList()), Node(4, emptyList())))))
+                .isEqualTo(listOf(1,3,5,6,2,4))
+    }
 }
