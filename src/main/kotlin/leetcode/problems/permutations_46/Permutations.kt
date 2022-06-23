56adc6039ec864d945f77201012cd00029c936a4
Xuemin Guan <xueminguan@xuemins-mbp.lan>
2022-06-23 15:16:39 +0100

    a bit of refactoring

--- src/main/kotlin/leetcode/problems/permutations_46/Permutations.kt
+++ src/main/kotlin/leetcode/problems/permutations_46/Permutations.kt
@@ -1,17 +1,16 @@
 package leetcode.problems.permutations_46
 
 internal class Solution {
-    fun permute(nums: List<Int>): List<List<Int>> {
-
-        return nums.foldIndexed(emptyList()) { index, acc, num ->
+    fun permute(nums: List<Int>): List<List<Int>> =
+        nums.foldIndexed(emptyList()) { index, acc, num ->
             val subList: List<Int> = nums.subList(0, index).plus(nums.subList(index + 1, nums.size))
             val subListPermutations = permute(subList)
             if (subListPermutations.isEmpty()) {
                 acc.plus(listOf(listOf(num)))
             } else {
-                val fullListPermutations: List<List<Int>> = subListPermutations.map { subListPermutation -> listOf(num) + subListPermutation }
+                val fullListPermutations: List<List<Int>> =
+                    subListPermutations.map { subListPermutation -> listOf(num) + subListPermutation }
                 acc.plus(fullListPermutations)
             }
         }
-    }
 }
