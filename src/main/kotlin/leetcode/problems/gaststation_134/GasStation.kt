e48faaf74eb7ef2e10d59cb591ab655db18398c5
Xuemin Guan <xueminguan@xuemins-mbp.lan>
2022-06-24 15:12:03 +0100

    Some refacotoring

--- src/main/kotlin/leetcode/problems/gaststation_134/GasStation.kt
+++ src/main/kotlin/leetcode/problems/gaststation_134/GasStation.kt
@@ -1,28 +1,34 @@
 package leetcode.problems.gaststation_134
 
 class Solution {
-    fun canCompleteCircuit(gasStations: IntArray, costs: IntArray): Int {
-
-        return gasStations.withIndex().find { (index, value) ->
+    fun canCompleteCircuit(gasStations: IntArray, costs: IntArray): Int =
+        gasStations.withIndex().find { (index, _) ->
             val circularGasStations = circularRoute(index, gasStations)
             val circularCosts = circularRoute(index, costs)
-            var tank = 0
-            var reachedIndex = 0
-            for ((index, gas) in circularGasStations.withIndex()) {
-                tank = tank + gas - circularCosts[index]
-                reachedIndex = if (tank < 0) {
-                    reachedIndex = index
-                    break
-                } else {
-                    reachedIndex = index
-                    continue
-                }
-            }
+            val reachedIndex = reachableIndex(circularGasStations, circularCosts)
 
             reachedIndex == circularGasStations.size - 1
         }?.let {
             it.index
         } ?: -1
+
+    private fun reachableIndex(
+        circularGasStations: List<Int>,
+        circularCosts: List<Int>
+    ): Int {
+        var tank = 0
+        var reachedIndex = 0
+        for ((index, gas) in circularGasStations.withIndex()) {
+            tank = tank + gas - circularCosts[index]
+            reachedIndex = if (tank < 0) {
+                reachedIndex = index
+                break
+            } else {
+                reachedIndex = index
+                continue
+            }
+        }
+        return reachedIndex
     }
 
     private fun circularRoute(index: Int, gasStations: IntArray): List<Int> {
